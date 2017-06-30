1dd840431bb67820e3f5a84f8d2696f8a3802a95
Ronualdo Maciel <raxmac@gmail.com>
2017-06-29 21:38:21 -0300

    Adds model specs

--- spec/models/mms_resource_spec.rb
+++ spec/models/mms_resource_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe MmsResource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#is_image?' do
+    context 'given an image mms resource' do
+      subject(:image_resource) { described_class.new(filename: 'test.jpg') }
+
+      it 'confirms its type' do
+        expect(image_resource.is_image?).to eq(true)
+      end
+    end
+
+    context 'given an video mms resource' do
+      subject(:image_resource) { described_class.new(filename: 'test.mov') }
+
+      it 'confirms its type' do
+        expect(image_resource.is_image?).to eq(false)
+      end
+    end
+  end
+
+  describe '#path' do
+    it 'returns path for the local file' do
+      resource = described_class.new(filename: 'test.jpg')
+      expect(resource.path).to eq('./public/test.jpg')
+    end
+  end
 end
